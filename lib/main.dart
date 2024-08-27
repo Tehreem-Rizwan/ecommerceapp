4f08de5344ac7633067b02f72377661a953a7f92
Tehreem Rizwan <its.tehreem2830@gmail.com>
2024-08-27 19:37:54 +0500

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:ecommerceapp/firebase_options.dart';
 import 'package:ecommerceapp/screens/navigationbar_Screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,7 +16,8 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: BottomNavBar());
-  }
+  Widget build(BuildContext context) => MultiProvider(
+      providers: [],
+      child:
+          MaterialApp(debugShowCheckedModeBanner: false, home: BottomNavBar()));
 }
