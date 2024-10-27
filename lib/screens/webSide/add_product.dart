921d97bbd40d12bf6038a4a37cf1a3e8f8c12d7b
Tehreem Rizwan <its.tehreem2830@gmail.com>
2024-10-27 23:21:57 +0500

    updated

--- lib/screens/webSide/add_product.dart
+++ lib/screens/webSide/add_product.dart
@@ -1,5 +1,9 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:io';
+import 'package:ecommerceapp/components/constants.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AddProductScreen extends StatefulWidget {
   static const String id = "addproduct";
@@ -10,9 +14,9 @@ class AddProductScreen extends StatefulWidget {
 
 class _AddProductScreenState extends State<AddProductScreen> {
   String? selectedvalue;
-
-  // Mock data for List<List<Product>>
-  // Replace this with actual data in your project
+  final imagePicker = ImagePicker();
+  List<XFile> images = [];
+  List<String> imageUrls = [];
   List<List<String>> selectedCategories = [
     ['Filtered Products'],
     ['Shoes'],
@@ -24,7 +28,6 @@ class _AddProductScreenState extends State<AddProductScreen> {
 
   // Method to extract category names (or you could use another relevant value)
   List<String> getCategoryNames() {
-    // You can modify this method to fit your actual data structure (Product class)
     return selectedCategories
         .map((categoryList) =>
             categoryList[0]) // Assuming each list has one category name
@@ -35,50 +38,167 @@ class _AddProductScreenState extends State<AddProductScreen> {
   Widget build(BuildContext context) {
     List<String> categoryNames = getCategoryNames();
 
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-          child: Container(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Center(
-                  child: Text(
-                    "ADD PRODUCT",
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                SizedBox(height: 20),
-                DropdownButtonFormField<String>(
-                  validator: (value) {
-                    if (value == null) {
-                      return "Category must be selected";
-                    }
-                    return null;
-                  },
-                  value: selectedvalue,
-                  items: categoryNames
-                      .map((e) => DropdownMenuItem<String>(
-                            value: e,
-                            child: Text(e),
-                          ))
-                      .toList(),
-                  onChanged: (value) {
-                    setState(() {
-                      selectedvalue = value;
-                    });
-                  },
-                  decoration: InputDecoration(
-                    labelText: "Select Category",
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-              ],
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Text(
+                "ADD PRODUCT",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
             ),
-          ),
+            SizedBox(height: 20),
+            Form(
+              // Wrap Dropdown in Form
+              child: DropdownButtonFormField<String>(
+                validator: (value) {
+                  if (value == null) {
+                    return "Category must be selected";
+                  }
+                  return null;
+                },
+                value: selectedvalue,
+                items: categoryNames
+                    .map((e) => DropdownMenuItem<String>(
+                          value: e,
+                          child: Text(e),
+                        ))
+                    .toList(),
+                onChanged: (value) {
+                  setState(() {
+                    selectedvalue = value;
+                  });
+                },
+                decoration: InputDecoration(
+                  labelText: "Select Category",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            SizedBox(height: 70),
+            Center(
+              child: SizedBox(
+                width: 600,
+                height: 50,
+                child: ElevatedButton(
+                  child: Text(
+                    "PICK IMAGES",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () {
+                    pickImage();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: kprimaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Center(
+              child: SizedBox(
+                width: 600,
+                height: 50,
+                child: ElevatedButton(
+                  child: Text(
+                    "UPLOAD IMAGES",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () {
+                    uploadImages();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: kprimaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              height: 200,
+              decoration: BoxDecoration(
+                color: Colors.grey.withOpacity(0.1),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 5,
+                ),
+                itemCount: images.length, // Change to imageUrls.length
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Stack(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black),
+                          ),
+                          child: Image.network(
+                            File(images[index].path).path,
+                            height: 100,
+                            width: 100,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {
+                            setState(() {
+                              images.removeAt(index);
+                              imageUrls.removeAt(
+                                  index); // Also remove from imageUrls
+                            });
+                          },
+                          icon: Icon(Icons.cancel_outlined),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
-      ],
+      ),
     );
+  }
+
+  pickImage() async {
+    final List<XFile>? pickImage = await imagePicker.pickMultiImage();
+    if (pickImage != null) {
+      setState(() {
+        images.addAll(pickImage);
+      });
+    } else {
+      print("no images selected");
+    }
+  }
+
+  Future postImages(XFile? imageFile) async {
+    String? urls;
+    Reference ref =
+        FirebaseStorage.instance.ref().child("images").child(imageFile!.name);
+
+    if (kIsWeb) {
+      await ref.putData(await imageFile.readAsBytes(),
+          SettableMetadata(contentType: "image/jpeg"));
+      urls = await ref.getDownloadURL();
+      return urls;
+    }
+  }
+
+  uploadImages() async {
+    for (var image in images) {
+      await postImages(image).then((downloadUrl) => imageUrls.add(downloadUrl));
+    }
   }
 }
